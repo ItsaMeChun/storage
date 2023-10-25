696df4c1cf783601383398c006915da9fa5d2019
Nguyen177013 <phannguyendoremon@gmail.com>
2023-10-25 15:35:28 +0700

    wtf I am doing

--- Service/imp/AuthorService.cs
+++ Service/imp/AuthorService.cs
@@ -1,6 +1,33 @@
-﻿namespace hcode.Service.imp
+﻿using hcode.DTO;
+using hcode.Entity;
+
+namespace hcode.Service.imp
 {
     public class AuthorService : IAuthorService
     {
+        public bool Add(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AuthorDTO> ListAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, AuthorDTO authorDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
