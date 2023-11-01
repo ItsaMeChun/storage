ac760c14a01822c12b59487e3cdbad3df6b63656
Long <91193095+ItsaMeChun@users.noreply.github.com>
2023-11-01 10:28:02 +0700

    update some Sauce

--- Service/imp/SauceService.cs
+++ Service/imp/SauceService.cs
@@ -1,10 +1,20 @@
-﻿using hcode.DTO;
+﻿using AutoMapper;
+using hcode.DTO;
 using hcode.Entity;
+using hcode.Repository;
 
 namespace hcode.Service.imp
 {
     public class SauceService : ISauceService
     {
+        private readonly IRepository<Sauce> _sauceRepository;
+        private readonly IMapper _mapper;
+
+        public SauceService(IRepository<Sauce> sauceRepository, IMapper mapper)
+        {
+            this._sauceRepository = sauceRepository;
+            this._mapper = mapper;
+        }
         public bool Add(SauceDTO sauce)
         {
             throw new NotImplementedException();
@@ -12,20 +22,33 @@ namespace hcode.Service.imp
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            //return _sauceRepository.Delete(id);
+            var sauce = _sauceRepository.FindById(id);
+            if(sauce == null)
+            {
+                // help, i got task
+                return false;
+            }
+
+            return true;
         }
 
         public Sauce Get(int id)
         {
-            throw new NotImplementedException();
+            return _sauceRepository.FindById(id);
         }
 
         public IEnumerable<Sauce> ListSauces()
         {
-            throw new NotImplementedException();
+            return _sauceRepository.GetAll();
         }
 
         public bool Update(int id, SauceDTO sauce)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Sauce FindSauce(SauceDTO sauceDto)
         {
             throw new NotImplementedException();
         }
